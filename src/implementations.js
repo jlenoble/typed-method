580fb345d3422b055dd8cbeb3db46524681b95e3
Jason Lenoble <jason.lenoble@gmail.com>
2018-01-02 18:12:59 +0100

    Extracted/optimized reciprocalImplementation

--- src/implementations.js
+++ src/implementations.js
@@ -1,5 +1,27 @@
 const _methods = new Map();
 
+export function getReciprocalImplementation (name, implementation) {
+  let reciprocalImplementation;
+
+  switch (implementation) {
+  case succeed: case fail:
+    reciprocalImplementation = implementation;
+    break;
+
+  default:
+    const _method = name + ':reciprocal';
+    reciprocalImplementation = _methods.get(_method);
+
+    if (!reciprocalImplementation) {
+      reciprocalImplementation = reciprocal(name);
+
+      _methods.set(_method, reciprocalImplementation);
+    }
+  }
+
+  return reciprocalImplementation;
+}
+
 export function getNegateImplementation (name, implementation, nCallees) {
   let negateImplementation;
 
@@ -13,21 +35,33 @@ export function getNegateImplementation (name, implementation, nCallees) {
     break;
 
   default:
-    const _method = name + `(${nCallees})`;
+    const _method = name + `:negate(${nCallees})`;
     negateImplementation = _methods.get(_method);
 
     if (!negateImplementation) {
-      negateImplementation = nCallees === 1 ? function (obj) {
-        return !this[name](obj); // eslint-disable-line no-invalid-this
-      } : function (...args) {
-        return !this[name](...args); // eslint-disable-line no-invalid-this
-      };
+      negateImplementation = negate(name, nCallees);
 
       _methods.set(_method, negateImplementation);
     }
   }
 
   return negateImplementation;
+}
+
+export function reciprocal (name) {
+  return function (obj) {
+    return obj[name](this); // eslint-disable-line no-invalid-this
+  };
+}
+
+export function negate (name, nCallees) {
+  return nCallees === 1
+    ? function (obj) {
+      return !this[name](obj); // eslint-disable-line no-invalid-this
+    }
+    : function (...args) {
+      return !this[name](...args); // eslint-disable-line no-invalid-this
+    };
 }
 
 export function succeed () {
--- src/reciprocal.js
+++ src/reciprocal.js
@@ -1,22 +1,20 @@
 import Binary from './binary';
-
-const _methods = new Map();
+import {getReciprocalImplementation} from './implementations';
 
 export default class Reciprocal {
   constructor (name, reciprocalName, implementation, callerType, calleeType) {
-    let reciprocal = _methods.get(name);
-
-    if (!reciprocal) {
-      function recip (obj) {
-        return obj[name](this); // eslint-disable-line no-invalid-this
-      }
-      _methods.set(name, recip);
-      reciprocal = recip;
-    }
+    const reciprocalImplementation = getReciprocalImplementation(
+      name, implementation
+    );
 
     new Binary(name, implementation, callerType, calleeType);
 
-    this._makeReciprocal(reciprocalName, reciprocal, callerType, calleeType);
+    this._makeReciprocal(
+      reciprocalName,
+      reciprocalImplementation,
+      callerType,
+      calleeType
+    );
   }
 
   _makeReciprocal (name, reciprocal, callerType, calleeType) {
